3d9442f58d5f5cfefc61e59ba59bb8ca7c964c52
Martynas Mickevičius <self@2m.lt>
2024-05-16 00:07:23 +0300

    convert to expected types

--- config.py
+++ config.py
@@ -9,6 +9,11 @@ def set_from_env():
     global refs
     for key, val in os.environ.items():
         if "RMM_" in key:
+            if val.isnumeric():
+                val = int(val)
+            elif val.lower() in ["true", "false"]:
+                val = val.lower() == "true"
+
             key = key.replace("RMM_", "").lower()
             refs[key] = val
 
--- justfile
+++ justfile
@@ -3,7 +3,11 @@ docker-build:
 
 docker-run:
     docker run -it --rm \
+        --privileged \
         --net=host \
         --name rpi-mqtt-monitor \
         --env RMM_MQTT_HOST=localhost \
+        --env RMM_MQTT_TOPIC_PREFIX=monitor \
+        --env RMM_UPDATE=false \
+        --env RMM_SERVICE_SLEEP_TIME=5 \
         rpi-mqtt-monitor
